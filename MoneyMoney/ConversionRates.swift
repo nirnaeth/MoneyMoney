9e01ceae05b7f77e22455437fe20b87c4dfd0766
Monica Giambitto <m.giambitto@freegoweb.it>
2021-03-11 10:03:43 +0100

    add picker for currencies

--- MoneyMoney/ConversionRates.swift
+++ MoneyMoney/ConversionRates.swift
@@ -7,10 +7,11 @@
 
 import Foundation
 
-enum Currency: String, Decodable {
+enum Currency: String, Decodable, CaseIterable {
     case USD
     case GBP
     case EUR
+    case YEN
 }
 
 // {"rates":{"USD":1.0,"GBP":0.734421365},"base":"USD","date":"2021-01-19"}}
--- MoneyMoney/ViewController.swift
+++ MoneyMoney/ViewController.swift
@@ -15,17 +15,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var appNameLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    private let currencies: [Currency] = Currency.allCases
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "MoneyMoney 💲"
+        // Type inference, therefore no need for specifying UIColor in front of the value
+        convertButton.backgroundColor = .orange
+        convertButton.layer.cornerRadius = 5
+        
     }
 
     @IBAction func convertButtonAction(_ sender: Any) {
         loadData()
-        
+    }
+    
+    @IBAction func selectCurrencyAction() {
+        currencyPicker.selectedRow(inComponent: 0)
     }
     
     func loadData() {
@@ -39,12 +50,25 @@ class ViewController: UIViewController {
     func updateUI(rate currentRate: ConversionRate) {
         let GBPConversionRate = currentRate.rates[.GBP] ?? 0
         let originalAmount = Double(self.textField.text ?? "") ?? 0
-        let convertedAmount = originalAmount * GBPConversionRate
+        let convertedAmount = String(format: "%.2f", originalAmount * GBPConversionRate) // TODO: round to 2 decimals
         self.resultLabel.text = "\(convertedAmount) \(Currency.GBP.rawValue)"
     }
 }
 
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        currencies.count
+    }
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        currencies[row].rawValue
+    }
+}
 
 // TODO: make it pretty - code & UI
-// TODO: round to 2 decimals
-// TODO: draw interface mockup
